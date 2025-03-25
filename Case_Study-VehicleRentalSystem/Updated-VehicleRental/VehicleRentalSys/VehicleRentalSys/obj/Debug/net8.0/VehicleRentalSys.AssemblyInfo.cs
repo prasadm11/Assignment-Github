ead00443e3b4d624550a3e2e6128473bb63ec46a
Prasad Mahajan <pmmahajan2002@gmail.com>
2025-03-25 12:19:04 +0530

    Updated Vehiclerentalsys with Bootstrap
    Updated Vehiclerentalsys with Bootstrap

--- Case_Study-VehicleRentalSystem/Updated-VehicleRental/VehicleRentalSys/VehicleRentalSys/obj/Debug/net8.0/VehicleRentalSys.AssemblyInfo.cs
+++ Case_Study-VehicleRentalSystem/Updated-VehicleRental/VehicleRentalSys/VehicleRentalSys/obj/Debug/net8.0/VehicleRentalSys.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleRentalSys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbdc430d357eb9399b6b3f3ff3e993bcd8b82062")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleRentalSys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleRentalSys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
